4588bb60ef7eec26def6e284e5c96d81ffd22cbb
Ramen96 <eric.worsfold@gmail.com>
2025-10-27 22:30:25 -0400

    updated main loop to use new player animation

--- app/game/page.jsx
+++ app/game/page.jsx
@@ -4,11 +4,11 @@ import { Application } from 'pixi.js';
 import { Player } from "./GameUtils/classes/player.js"; 
 import { InputManager } from "./GameUtils/classes/inputManager.js";
 import { PhysicsEngine } from "./GameUtils/classes/physicsEngine.js";
-import { knightAnimations } from "./GameUtils/Animations/playerKnight.js";
+import { playerAnimations } from "./GameUtils/Animations/player.js";
 
 export default function Game() {
   useEffect(() => {
-    let app, knight, inputManager, physicsEngine;
+    let app, player, inputManager, physicsEngine;
 
     (async () => {
       // Application setup
@@ -21,29 +21,29 @@ export default function Game() {
       inputManager.startListening();
 
       // character setup
-      knight = new Player();
-      await knight.loadAnimations(knightAnimations);
+      player = new Player();
+      await player.loadAnimations(playerAnimations);
       
-      const groundY = app.screen.height - knight.characterDimensions.height;
+      const groundY = app.screen.height - player.characterDimensions.height;
       
       // Set spawn position
-      knight.setPosition(app.screen.width / 2, groundY); 
+      player.setPosition(app.screen.width / 2, groundY); 
 
       // physics engine setup
       physicsEngine = new PhysicsEngine(
-        knight, 
+        player, 
         app.screen.height,
         app.screen.width 
       );
 
       // Link InputManager to the Player's movement properties
       const updateMovementState = (keys) => {
-          Object.assign(knight.movement, keys);
+          Object.assign(player.movement, keys);
       };
       inputManager.subscribe(updateMovementState);
       
       // stage setup: Add the player's animated sprites to the stage
-      Object.values(knight.animations).forEach(animation => {
+      Object.values(player.animations).forEach(animation => {
         app.stage.addChild(animation);
       });
       
